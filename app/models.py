7547bf50bf72523a9df98b27d84969a769431938
ilkay kinaci <ilkaykinaci@gmail.com>
2026-04-14 10:33:55 +0300

    full unified system

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from app.db import Base
+
+class Question(Base):
+    __tablename__ = "questions"
+    id = Column(Integer, primary_key=True, index=True)
+    question = Column(Text, nullable=False)
+    option_a = Column(String(255), nullable=False)
+    option_b = Column(String(255), nullable=False)
+    option_c = Column(String(255), nullable=False)
+    option_d = Column(String(255), nullable=False)
+    correct = Column(Integer, nullable=False)
